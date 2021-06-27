a17b5c352e0227f85ae00d9ab55dac52a6d03ae3
Marcus14119 <60606347+Marcus14119@users.noreply.github.com>
2021-06-27 15:36:52 +0300

    master

---
+++ lib/util/app_url.dart
@@ -0,0 +1,14 @@
+
+
+class AppUrl {
+  static const String liveBaseURL = "https://payherokenya.com/singlepay/app/";
+  
+  static const String baseURL = liveBaseURL;
+  static const String register = baseURL + "individual_registration.php";
+  static const String otp_verification = baseURL + "otp_verification.php";
+  static const String business_registration = baseURL + "business_registration.php";
+  static const String login = baseURL + "authentication.php";
+  static const String forgotPassword = baseURL + "forgot_password.php";
+  static const String changePassword = baseURL + "change_password.php";
+}
+
